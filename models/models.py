2ffe1cfcde7e6dd7a89463303c03eeeee23d3cbe
Adel27 <r-27adel@live.fr>
2016-01-20 21:08:38 +0100

    Module internationalization

--- models/models.py
+++ models/models.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from datetime import timedelta
-from openerp import models, fields, api, exceptions
+from openerp import models, fields, api, exceptions, _
 
 class Course(models.Model):
     _name = 'openacademy.course'
+
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-    
+
     responsible_id = fields.Many2one('res.users',
             ondelete='set null', string="Responsible", index=True)
 
@@ -19,11 +20,11 @@ class Course(models.Model):
         default = dict(default or {})
 
         copied_count = self.search_count(
-            [('name', '=like', u"Copy of {}%".format(self.name))])
+            [('name', '=like', _(u"Copy of {}%").format(self.name))])
         if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
+            new_name = _(u"Copy of {}").format(self.name)
         else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
 
         default['name'] = new_name
         return super(Course, self).copy(default)
@@ -42,7 +43,7 @@ class Course(models.Model):
 
 class Session(models.Model):
     _name = 'openacademy.session'
-    
+
     name = fields.Char(required=True)
     start_date = fields.Date(default=fields.Date.today)
 
@@ -57,7 +58,7 @@ class Session(models.Model):
 
     course_id = fields.Many2one('openacademy.course',
                             ondelete='cascade', string="Course", required=True)
-    
+
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
@@ -96,24 +97,24 @@ class Session(models.Model):
                 r.taken_seats = 0.0
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
-            
+
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
         if self.seats < 0:
             return {
                 'warning': {
-                    'title': "Incorrect 'seats' value",
-                    'message': "The number of available seats may not be negative",
+                    'title': _("Incorrect 'seats' value"),
+                    'message': _("The number of available seats may not be negative"),
                 },
             }
         if self.seats < len(self.attendee_ids):
             return {
                 'warning': {
-                    'title': "Too many attendees",
-                    'message': "Increase seats or remove excess attendees",
+                    'title': _("Too many attendees"),
+                    'message': _("Increase seats or remove excess attendees"),
                 },
             }
-            
+
     @api.depends('start_date', 'duration')
     def _get_end_date(self):
         for r in self:
@@ -158,5 +159,4 @@ class Session(models.Model):
     def _check_instructor_not_in_attendees(self):
         for r in self:
             if r.instructor_id and r.instructor_id in r.attendee_ids:
-                raise exceptions.ValidationError("A session's instructor can't be an attendee")
-
+                raise exceptions.ValidationError(_("A session's instructor can't be an attendee"))
